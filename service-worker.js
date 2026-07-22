299304202bb9fcb749e5f1a2bc08f274952bd1b9
Krishna <krishna@local>
2026-07-22 09:29:31 +0530

    Fix AI chat message visibility

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'journall-android-pwa-v19';
+﻿const CACHE_NAME = 'journall-android-pwa-v20';
 const APP_SHELL = [
   './',
   './index.html',
@@ -77,3 +77,4 @@ self.addEventListener('fetch', event => {
     })
   );
 });
+
